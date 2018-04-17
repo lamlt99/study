f44286a5f8a3a25431471d7dc1384f377d6f1a8d
Tung Lam <tunglammi2@gmail.com>
2018-04-17 22:04:34 +0700

    reupdate class v1.0

---
+++ class 1.0/cmake-build-debug/CMakeFiles/class_1_0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class_1_0.dir/main.cpp.obj"
+  "class_1_0.pdb"
+  "class_1_0.exe"
+  "class_1_0.exe.manifest"
+  "libclass_1_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class_1_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
